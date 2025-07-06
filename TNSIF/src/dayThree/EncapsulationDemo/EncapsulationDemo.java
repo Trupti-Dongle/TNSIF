f7ed6b463de100900fd1c9b8108dd5e7ad3af309
TRUPTI <TRUPTI@TRUPTI>
2025-07-07 00:57:34 +0530

    DayThree

---
+++ TNSIF/src/dayThree/EncapsulationDemo/EncapsulationDemo.java
@@ -0,0 +1,17 @@
+package dayThree.EncapsulationDemo;
+
+public class EncapsulationDemo {
+	
+
+	public static void main(String[] args) {
+
+		OopsConceptDemo obj=new OopsConceptDemo();
+		
+		obj.setName("Shubham");
+		obj.setAge(23);
+		obj.setSerailNum(1001);
+		
+		System.out.println(obj);
+	}
+
+}
