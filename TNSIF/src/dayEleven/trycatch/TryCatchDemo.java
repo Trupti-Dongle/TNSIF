d4f97456b3d16e4225e1ef585c8f0fbb4f7de716
TRUPTI <TRUPTI@TRUPTI>
2025-07-10 10:20:08 +0530

    Exception Handling

---
+++ TNSIF/src/dayEleven/trycatch/TryCatchDemo.java
@@ -0,0 +1,15 @@
+package dayEleven.trycatch;
+
+public class TryCatchDemo {
+	
+	public static void main(String[] args) {
+		
+		
+		TryCatchExample.performDivision(90, 30);
+		TryCatchExample.performDivision(80, 40);
+		TryCatchExample.performDivision(100, 0);
+		TryCatchExample.performDivision(7800, 30);
+		TryCatchExample.performDivision(60, 0);
+	}
+
+}
